078ed00d34badfa35883d75360a05a4f787959da
Ankush-gt <124658878+Ankush-gt@users.noreply.github.com>
2023-03-13 19:16:05 +0530

    Add files via upload

--- SMWS/index.php
+++ SMWS/index.php
@@ -5,42 +5,79 @@ $outPut = "";
 if(isset($_POST['submit'])) {
   $userName = $_POST['email'];
   $userPassword = $_POST['uPassword'];
+  session_start();
   $outPut=$auth->login($userName,$userPassword);
-  //  if(!empty($userName) || !empty($userPassword)) {
-  //   $outPut= "missing input";
-  //  } else {
-  //    $input=$auth->login($userName,$userPassword);
-  //    $displayError = $auth->login($userName, $userPassword);
-  // } 
-  // print_r($outPut);
+
+  
   if($outPut) {
+    echo "Done";
+     $_SESSION['email'] = $userName;
+     $_SESSION['uPassword'] = $userPassword;
+    
+
+
     header("Location: Dashboard.php");
+
+
+      echo "<br>";
+
+     exit();
+  }
+
   } else {
     $outPut = "incorrect information";
   }
-}
+
+// if (isset($_POST['submit'])) {
+//   $email = $_POST['email'];
+//   $password = $_POST['uPassword'];
+
+//   $sql = "select * from user where email = '$email' and  uPassword = '$password' ";
+//   $query = $conn->query($sql);
+
+//   $email_count = mysqli_num_rows($query);
+
+//   if ($email_count) {
+//     $email_pass = mysqli_fetch_assoc($query);
+
+//     $db_pass = $email_pass['password'];
+
+//     $pass_decode = password_verify($uPassword, $db_pass);
+
+//     if ($pass_decode) {
+//       echo " login succesfully";
+//     } else {
+//       echo " incorrect";
+//     }
+//   } else {
+//     echo " invalid";
+//   }
+
+// }
+
+
+
 ?>
 <div class="login-form">
-    <form method="post" action="index.php">
-      <h1>Login</h1>
-      <div class="content">
-        <div class="input-field">
-          <input type="email" placeholder="email" autocomplete="nope" name="email">
-        </div>
-        <div class="input-field">
-          <input type="password" placeholder="uPassword" autocomplete="new-password" name="uPassword">
-        </div>
-        <a href="./forgotpass.php" class="text-body">Forgot password?</a>
-        <div class="input-field">
-        </div>
+  <form method="post" action="index.php">
+    <h1>Login</h1>
+    <div class="content">
+      <div class="input-field">
+        <input type="email" placeholder="email" autocomplete="nope" name="email">
       </div>
-      <div class="action">
-      <input type="submit"  name="submit" value="Login"></button>
-      <?php echo $outPut ? ($outPut) : ("");?>
-
-        <!-- <button>Register</a></button>  -->
-        
+      <div class="input-field">
+        <input type="password" placeholder="uPassword" autocomplete="new-password" name="uPassword">
       </div>
-    </form>
-  </div>
+      <a href="./forgotpass.php" class="text-body">Forgot password?</a>
+      <div class="input-field">
+      </div>
+    </div>
+    <div class="action">
+      <input type="submit" name="submit" value="Login"></button>
+      <?php echo $outPut ? ($outPut) : (""); ?>
 
+      <!-- <button>Register</a></button>  -->
+
+    </div>
+  </form>
+</div>

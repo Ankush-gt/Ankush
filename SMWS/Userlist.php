1887c44274fe896b24c06d9aa3a397ddf52c940d
Ankush-gt <124658878+Ankush-gt@users.noreply.github.com>
2023-03-13 19:14:39 +0530

    Add files via upload

---
+++ SMWS/Userlist.php
@@ -0,0 +1,16 @@
+<?php
+include('./public/meta.php');
+include('./public/header.php');
+include('./user.php');
+
+$user = new user();
+$user_list_arr = $user->list();
+
+// echo "<pre>";
+// print_r($user_list_arr);
+
+foreach($user_list_arr as $key => $value) {
+    echo $value['uId'] .  ". Welcome ". $value['firstName'] ." ". $value['lastName'];
+            echo "<br>";
+}
+?>

99a2ac3a4c9aa8f2007959dc80ee04288613b8bd
Ankush-gt <124658878+Ankush-gt@users.noreply.github.com>
2023-03-15 19:29:29 +0530

    Add files via upload

--- SMWS/user.php
+++ SMWS/user.php
@@ -1,58 +1,73 @@
 <?php
-class user{
-    function login(){
+include("./include/database.php");
+
+class user
+{
+    function login()
+    {
         $con = new mysqli(DBHOSTNAME, DBUSERNAME, DBPASSWORD, DBNAME);
         if ($con->connect_error) {
             die("Connection failed: " . $con->connect_error);
         }
 
-        if (isset($_SESSION['email']) && isset($_SESSION['uPassword']))  {
+        if (isset($_SESSION['email']) && isset($_SESSION['uPassword'])) {
             $existingUserMail = $_SESSION['email'];
             $existingpassword = $_SESSION['uPassword'];
         } else {
             die("Not found data");
         }
-
         $que = "SELECT * FROM user WHERE email = '$existingUserMail' AND uPassword = '$existingpassword'";
         $info = $con->query($que);
         $row = mysqli_fetch_assoc($info);
         return $row;
     }
 
-    function list(){
-        $con = new mysqli(DBHOSTNAME, DBUSERNAME, DBPASSWORD, DBNAME);
+    function list()
+    {
+        global $conn;
         $sql = "SELECT * FROM `user`";
-        $result = mysqli_query($con, $sql);
-   //   *******Records********
-        $num = mysqli_num_rows($result);     
+        $result = mysqli_query($conn, $sql);
+        $num = mysqli_num_rows($result);
         echo $num;
-        echo " records found in the DataBase<br>";   
+        echo " records found in the DataBase<br>";
         echo "<br>";
-       if($num>0){
-    //    return mysqli_fetch_assoc($result);
-            // $row = mysqli_fetch_assoc($result);
-           //  $row = mysqli_fetch_assoc($result);
-           // echo var_dump($row);
-        //    print_r($user_list_arr);
-           while($row = mysqli_fetch_assoc($result)){
-              $arr[] = $row;
+        if ($num > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $arr[] = $row;
             }
             return $arr;
-       }else{
-        return 0;
-       }
+        } else {
+            return 0;
+        }
     }
-    // function get($userId){
-    //     if($num> 0){
-    //         $num = mysqli_num_rows($result);     
-    //         echo $num;
-    //         $row = mysqli_fetch_assoc($result);
-    //         echo var_dump($row);
+    function get($userId)
+    {
+        global $conn;
+        $query = "SELECT *FROM user where uId ='$userId'";
+        $result2 = $conn->query($query);
+        $row = mysqli_fetch_array($result2);
+        return $row;
+    }
+    // *************************************************************
+    function add($firstName, $lastname, $email, $uPassword)
+    {
+        global $conn;
+        $query = "INSERT INTO user ( `firstName`,`lastName`,`email`,`uPassword`) VALUES ('$firstName','$lastname','$email','$uPassword')";
+        $result = $conn->query($query);
+        if ($result) {
+            $sql = "SELECT uId FROM user WHERE email = $email And uPassword = $uPassword";
+            $row = $conn->query($query);
+            $id = mysqli_fetch_assoc($row);
+            echo "Added sucessfully";
+            return $id;
+            } else {
+            return false;
+        }
 
-    //     }
-    // }
-
+    }
 }
+
+
 $userInfo = new User();
 
 ?>

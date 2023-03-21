8edae568cfc13b32c78247cc36887b02de8dfab2
Ankush-gt <124658878+Ankush-gt@users.noreply.github.com>
2023-03-21 18:42:23 +0530

    Add files via upload

---
+++ SMWS/include/config.php
@@ -0,0 +1,19 @@
+<?php
+
+include($_SERVER['DOCUMENT_ROOT'].'/smws/liabrary/helper.php');
+include($_SERVER['DOCUMENT_ROOT'].'/smws/liabrary/auth.php');
+include($_SERVER['DOCUMENT_ROOT'].'/smws/liabrary/user.php');
+include($_SERVER['DOCUMENT_ROOT'].'/smws/liabrary/apart.php');
+
+
+
+$userInfo = new user();
+
+$auth= new Auth();
+
+$apt1 = new apartment();
+
+// $apt2 = new apartment();
+// $apartment = new apart();
+
+?>
